c8c43a385c23963c2dcb6aff9a3d9d5907d26e55
Alec Byrd <byrdap@pitzer-rw01.ten.osc.edu>
2024-11-07 13:04:34 -0500

    .gitignore files

--- homework6/hw6_slurm.sh
+++ homework6/hw6_slurm.sh
@@ -8,19 +8,19 @@
 #SBATCH --time=00:10:00
 #SBATCH --mem=1GB
 #SBATCH --nodes=1
-#SBATCH --tasks-per-node=8
+#SBATCH --tasks-per-node=1
 
 # benchmark about 100 seconds
 
 # You may uncomment the following line to compile the program
- g++ -g -Wall -std=c++17 -O3 main.cpp PNG.cpp PNG.h Assert.h -o ImageChecker -fopenmp -lpng
+g++ -g -Wall -std=c++17 -O3 main.cpp PNG.cpp PNG.h Assert.h -o ImageChecker -fopenmp -lpng
 
 #set core count to x
-export OMP_NUM_THREADS=8
+export OMP_NUM_THREADS=1
 
-# perf record -F 20 --call-graph fp ./ImageChecker images/Mammogram.png  images/Cancer_mask.png outputs/cancerout.png
+perf record -F 20 --call-graph fp ./ImageChecker images/Mammogram.png  images/Cancer_mask.png outputs/cancerout.png
 
 # Run the script
-/usr/bin/time -v ./ImageChecker images/Mammogram.png  images/Cancer_mask.png outputs/cancerout.png true 75 32
+# /usr/bin/time -v ./ImageChecker images/Mammogram.png  images/Cancer_mask.png outputs/cancerout.png true 75 32
 
 #end of script
